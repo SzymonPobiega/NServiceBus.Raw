102d232a1f17ab1c6081707f3fb8466f1969d5bf
SzymonPobiega <szymon.pobiega@gmail.com>
2020-06-09 21:28:04 +0200

    Add SQS acceptance tests.

---
+++ src/AcceptanceTests.SQS/When_sending_from_send_only_endpoint_ASQ.cs
@@ -0,0 +1,11 @@
+﻿using NServiceBus;
+using NUnit.Framework;
+
+[TestFixture]
+public class When_sending_from_send_only_endpoint_ASQ : When_sending_from_send_only_endpoint<SqsTransport>
+{
+    protected override void SetupTransport(TransportExtensions<SqsTransport> extensions)
+    {
+        extensions.ConfigureSQS();
+    }
+}
