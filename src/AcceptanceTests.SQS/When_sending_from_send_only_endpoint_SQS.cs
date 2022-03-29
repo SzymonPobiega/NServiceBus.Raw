bb05856b5a2ee1ca06153e8b5d17ead9fe9e2bff
Andreas Öhlund <andreas.ohlund@particular.net>
2022-03-29 08:59:08 +0200

    Enabling sqs

---
+++ src/AcceptanceTests.SQS/When_sending_from_send_only_endpoint_SQS.cs
@@ -0,0 +1,12 @@
+﻿using NServiceBus;
+using NServiceBus.Transport;
+using NUnit.Framework;
+
+[TestFixture]
+public class When_sending_from_send_only_endpoint_SQS : When_sending_from_send_only_endpoint<SqsTransport>
+{
+    protected override TransportDefinition SetupTransport()
+    {
+        return new SqsTransport();
+    }
+}
---
+++ src/AcceptanceTests.SQS/When_sending_to_another_endpoint_SQS.cs
@@ -0,0 +1,12 @@
+﻿using NServiceBus;
+using NServiceBus.Transport;
+using NUnit.Framework;
+
+[TestFixture]
+public class When_sending_to_another_endpoint_SQS : When_sending_to_another_endpoint<SqsTransport>
+{
+    protected override TransportDefinition SetupTransport()
+    {
+        return new SqsTransport();
+    }
+}
